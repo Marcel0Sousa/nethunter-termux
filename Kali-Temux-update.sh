22c0393388d87544fff3e15b06195b0d15f74d7f
tchelospy <tchelospy@gmail.com>
2017-10-26 17:48:15 -0200

    Update Kali-Temux-update.sh

--- Kali-Temux-update.sh
+++ Kali-Temux-update.sh
@@ -106,6 +106,7 @@ echo "installed successfully.... :D"
 echo
 echo "Now Kali Nethunter Is Installing......."
 echo
+#Kali oficial
 wget https://build.nethunter.com/kalifs/kalifs-latest/kalifs-arm64-minimal.tar.xz
 echo
 proot --link2symlink tar -xf kalifs-arm64-minimal.tar.xz
@@ -130,9 +131,12 @@ echo "installed successfully.... :D"
 echo
 echo "Now Kali Nethunter Is Installing......."
 echo
-wget https://build.nethunter.com/kalifs/kalifs-latest/kalifs-armhf-minimal.tar.xz
+#Kali test
+wget http://download662.mediafire.com/nmp2n9lahnag/f63bor2f7f66yvh/kali-armhf.tar.gz
+#Kali Oficial
+# wget https://build.nethunter.com/kalifs/kalifs-latest/kalifs-armhf-minimal.tar.xz
 echo
-proot --link2symlink tar -xf kalifs-armhf-minimal.tar.xz
+proot --link2symlink tar -xf kali-armhf.tar.gz # kalifs-armhf-minimal.tar.xz
 cd kali-armhf && echo "nameserver 8.8.8.8" > etc/resolv.conf
 
 cd ../ && echo "proot --link2symlink -0 -r kali-armhf -b /dev/ -b /sys/ -b /proc/ -b /data/data/com.termux/files/home -b /system -b /mnt /usr/bin/env -i HOME=/root PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games TERM=$TERM /bin/bash --login" > startkali.sh
