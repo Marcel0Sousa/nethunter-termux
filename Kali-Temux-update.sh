0dae125e9d2b5fc786cef129c02e0ff843bd5f44
tchelospy <tchelospy@gmail.com>
2017-11-26 09:57:47 -0300

    Update Kali-Temux-update.sh

--- Kali-Temux-update.sh
+++ Kali-Temux-update.sh
@@ -63,11 +63,11 @@ proot --link2symlink tar -xf kalifs-arm64-full.tar.xz
 cd kali-arm64 && echo "nameserver 8.8.8.8" > etc/resolv.conf
 
 cd ../ && echo "proot --link2symlink -0 -r kali-arm64 -b /dev/ -b /sys/ -b /proc/ -b /data/data/com.termux/files/home -b /system -b /mnt /usr/bin/env -i HOME=/root PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games TERM=$TERM /bin/bash --login" > startkali.sh
-
+echo
 chmod 777 startkali.sh && termux-fix-shebang startkali.sh
-
-cat startkali.sh > /data/data/com.termux/files/usr/bin/kali
-chmod 700 /data/data/com.termux/files/usr/bin/kali
+echo
+#cat startkali.sh > /data/data/com.termux/files/usr/bin/kali
+#chmod 700 /data/data/com.termux/files/usr/bin/kali
 echo
 echo 
 echo "Now You Can Start Kali Linux (Nethunter) By :--> ./startkali.sh or kali"
@@ -87,11 +87,11 @@ proot --link2symlink tar -xf kalifs-armhf-full.tar.xz
 cd kali-armhf && echo "nameserver 8.8.8.8" > etc/resolv.conf
 
 cd ../ && echo "proot --link2symlink -0 -r kali-armhf -b /dev/ -b /sys/ -b /proc/ -b /data/data/com.termux/files/home -b /system -b /mnt /usr/bin/env -i HOME=/root PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games TERM=$TERM /bin/bash --login" > startkali.sh
-
+echo
 chmod 777 startkali.sh && termux-fix-shebang startkali.sh
-
-cat startkali.sh > /data/data/com.termux/files/usr/bin/kali
-chmod 700 /data/data/com.termux/files/usr/bin/kali
+echo
+#cat startkali.sh > /data/data/com.termux/files/usr/bin/kali
+#chmod 700 /data/data/com.termux/files/usr/bin/kali
 echo
 echo 
 echo "Now You Can Start Kali Linux (Nethunter) By :--> ./startkali.sh or kali"
@@ -112,11 +112,11 @@ proot --link2symlink tar -xf kalifs-arm64-minimal.tar.xz
 cd kali-arm64 && echo "nameserver 8.8.8.8" > etc/resolv.conf
 
 cd ../ && echo "proot --link2symlink -0 -r kali-arm64 -b /dev/ -b /sys/ -b /proc/ -b /data/data/com.termux/files/home -b /system -b /mnt /usr/bin/env -i HOME=/root PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games TERM=$TERM /bin/bash --login" > startkali.sh
-
+echo
 chmod 777 startkali.sh && termux-fix-shebang startkali.sh
-
-cat startkali.sh > /data/data/com.termux/files/usr/bin/kali
-chmod 700 /data/data/com.termux/files/usr/bin/kali
+echo
+#cat startkali.sh > /data/data/com.termux/files/usr/bin/kali
+#chmod 700 /data/data/com.termux/files/usr/bin/kali
 echo
 echo 
 echo "Now You Can Start Kali Linux (Nethunter) By :--> ./startkali.sh or kali"
@@ -137,11 +137,11 @@ proot --link2symlink tar -xf kali-armhf.tar.gz
 cd kali-armhf && echo "nameserver 8.8.8.8" > etc/resolv.conf
 
 cd ../ && echo "proot --link2symlink -0 -r kali-armhf -b /dev/ -b /sys/ -b /proc/ -b /data/data/com.termux/files/home -b /system -b /mnt /usr/bin/env -i HOME=/root PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games TERM=$TERM /bin/bash --login" > startkali.sh
-
+echo
 chmod 777 startkali.sh && termux-fix-shebang startkali.sh
-
-cat startkali.sh > /data/data/com.termux/files/usr/bin/kali
-chmod 700 /data/data/com.termux/files/usr/bin/kali
+echo
+#cat startkali.sh > /data/data/com.termux/files/usr/bin/kali
+#chmod 700 /data/data/com.termux/files/usr/bin/kali
 echo
 echo 
 echo "Now You Can Start Kali Linux (Nethunter) By :--> ./startkali.sh or kali"
